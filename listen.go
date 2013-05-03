01223667868c0965b358499b6ff57be85bd2e7dc
GordonDiggs <gordon@gordondiggs.com>
2013-05-02 22:55:56 -0400

    some cleanup

--- listen.go
+++ listen.go
@@ -7,7 +7,31 @@ import (
 	"net/http"
 )
 
-const RANDOM_URL = "https://x-vinyl.herokuapp.com/items/random.json"
+const (
+	GOOGLE_URL = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&start=1&q="
+	RANDOM_URL = "https://x-vinyl.herokuapp.com/items/random.json"
+	INDEX      = `
+    <html>
+      <head>
+        <title>What should I listen to?</title>
+        <style type='text/css'>
+          body { background: black; color: white; font-size: 36px; text-align:center; font-family: sans-serif; }
+          em { color: yellow; }
+          strong { color: red; }
+        </style> 
+      </head>
+      <body>
+        <p>
+          You should listen to <em>{{.Title}}</em>
+          <br>
+          by <strong>{{.Artist}}</strong> on {{.Label}}.
+          <br>
+          It is a {{.Format}} record.
+        </p>
+      </body>
+    </html>
+  `
+)
 
 type Record struct {
 	Title  string
@@ -16,25 +40,13 @@ type Record struct {
 	Label  string
 }
 
-const index = `
-  <html>
-    <head>
-      <title>What should I listen to?</title>
-      <style type='text/css'>
-        body { background: black; color: white; font-size: 36px; text-align:center; font-family: sans-serif; }
-      </style> 
-    </head>
-    <body>
-      <p>
-        You should listen to <em>{{.Title}}</em>
-        <br>
-        by <strong>{{.Artist}}</strong> on {{.Label}}.
-        <br>
-        It is a {{.Format}} record.
-      </p>
-    </body>
-  </html>
-`
+/*
+func getAlbumArt(r Record) string {
+	q := url.QueryEscape(fmt.Sprintf("%s %s", r.Artist, r.Title))
+	req_url := fmt.Sprintf("%s%s", GOOGLE_URL, q)
+	return req_url
+}
+*/
 
 func GetRecord() Record {
 	var r Record
@@ -47,7 +59,7 @@ func GetRecord() Record {
 func handler(w http.ResponseWriter, r *http.Request) {
 	record := GetRecord()
 	t := template.New("Record Template")
-	t, _ = t.Parse(index)
+	t, _ = t.Parse(INDEX)
 	t.Execute(w, record)
 }
 
